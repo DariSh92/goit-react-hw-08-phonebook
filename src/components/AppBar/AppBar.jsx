324e7369a7d1ac9258e14db7e578ca82c8bb4ada
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2022-12-17 17:01:45 +0200

    initial commit

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,15 @@
+
+
+// import { Header } from "components/AppBar/AppBar.styled";
+
+
+// export const AppBar = () => {
+//     const { isLoggedIn } = useAuth();
+//     return (
+//         <Header>
+//       <Navigation />
+//       {isLoggedIn ? <UserMenu /> : <AuthNav />}
+//     </Header>
+//     )
+
+// }
